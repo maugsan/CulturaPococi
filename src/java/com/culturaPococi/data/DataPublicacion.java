08ce0b5c425b5354afcdff1902b1bb294264b269
carolizbm <carolizbm@gmail.com>
2013-04-22 09:31:45 -0600

    paquetes data

---
+++ src/java/com/culturaPococi/data/DataPublicacion.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.culturaPococi.data;
+
+/**
+ *
+ * @author jonathan
+ */
+public class DataPublicacion {
+    
+}
