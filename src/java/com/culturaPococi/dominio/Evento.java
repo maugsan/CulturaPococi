caeb6ae61207a5d963f157c9f12a7b19768a4109
carolizbm <carolizbm@gmail.com>
2013-05-07 11:05:47 -0600

    dominio: evento

--- src/java/com/culturaPococi/dominio/Evento.java
+++ src/java/com/culturaPococi/dominio/Evento.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,10 +12,89 @@ package com.culturaPococi.dominio;
 public class Evento {
 
     private int idEvento;
+    private int idCategoria;
     private String lugar;
-    //fecha datetime,
-    //hora time,
+    private String  fecha;
+    private String hora;
     private String informacion;
     private String correo;
-    //imagen blob,
+    private String imagen;
+
+    public Evento(int idEvento, int idCategoria, String lugar, String fecha, String hora, String informacion, String correo, String imagen) {
+        this.idEvento = idEvento;
+        this.idCategoria = idCategoria;
+        this.lugar = lugar;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.informacion = informacion;
+        this.correo = correo;
+        this.imagen = imagen;
+    }
+
+    public int getIdEvento() {
+        return idEvento;
+    }
+
+    public void setIdEvento(int idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getInformacion() {
+        return informacion;
+    }
+
+    public void setInformacion(String informacion) {
+        this.informacion = informacion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+    
+    
+    
 }
