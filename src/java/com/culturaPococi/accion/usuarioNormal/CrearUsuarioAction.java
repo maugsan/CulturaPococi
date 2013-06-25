15aec3df022c7c5c62920ee7a12ac9e56e6b7893
Juliana <ybarboza27@gmail.com>
2013-06-24 20:11:25 -0600

    prueba

--- src/java/com/culturaPococi/accion/usuarioNormal/CrearUsuarioAction.java
+++ src/java/com/culturaPococi/accion/usuarioNormal/CrearUsuarioAction.java
@@ -9,7 +9,6 @@ import com.culturaPococi.dominio.hibernate.UsuarioNormal;
 import com.culturaPococi.hibernate.helper.HelperUsuario;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.JOptionPane;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -38,26 +37,19 @@ public class CrearUsuarioAction extends DispatchAction {
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-//        UsuarioNormal un = new UsuarioNormal();
         HelperUsuario hu = new HelperUsuario();
         String correo = request.getParameter("correo");
         String contrasenia = request.getParameter("contrasenia");
         String colaborador = request.getParameter("colaborador");
         String contraseniaComprobacion=request.getParameter("contraseniaComprobacion");
-        JOptionPane.showMessageDialog(null, "correo action "+correo);
         if (colaborador == null) {
             colaborador = "n";
         }
 
-        
-     //  UsuarioNormal un = new UsuarioNormal(correo, "", contrasenia, contraseniaComprobacion , colaborador, "");
-        System.out.println("****************************************");
-      //  hu.getInsertUsuario(un);
-UsuarioNormal nu = new UsuarioNormal(correo, "", contrasenia, contraseniaComprobacion , colaborador, "");
-System.out.println("************************111111111************22222222****");      
-hu.getInsertUsuario(nu);
-        System.out.println("************************111111111****************");
-return mapping.getInputForward();
+    UsuarioNormal nu = new UsuarioNormal(correo, "", contrasenia, contraseniaComprobacion , colaborador, "");    
+    hu.getInsertUsuario(nu);
+
+    return mapping.getInputForward();
 
     }
 }
